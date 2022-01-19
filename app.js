9f0b7a1c1d0f3cba311ea3dd765012155f06bec5
kodmeron <89447350+kodmeron@users.noreply.github.com>
2022-01-19 15:59:03 +0100

    ajax klient

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+const cookieStorage = {
+    getItem: (key) => {
+        const cookies = document.cookie
+            .split(';')
+            .map(cookie => cookie.split('='))
+            .reduce((acc, [key, value]) => ({ ...acc, [key.trim()]: value }), {})
+        return cookies[key]
+    },
+    setItem: (key, value) => {
+        document.cookie = `${key}=${value}`
+    }
+}
+
+const storageType = cookieStorage
+const consentPropertyName = 'jdc_consent'
+
+const shouldShowPopup = () => !storageType.getItem(consentPropertyName)
+const saveToStorage = () => storageType.setItem(consentPropertyName, true)
 
 
 const cookieContainer = document.querySelector(".cookie-container")
@@ -8,19 +26,34 @@ cookieBtn.addEventListener("click", () => {
     localStorage.setItem("cookieBannerDisplayed", "true")
 });
 
-
-setTimeout( ()=>{
-    if(!localStorage.getItem("cookieBannerDisplayed"))
-    cookieContainer.classList.add("active")
+setTimeout(() => {
+    if (!localStorage.getItem("cookieBannerDisplayed"))
+        cookieContainer.classList.add("active")
 }, 2000);
 
-document.addEventListener("submit" , (event)=>{
+
+window.onload = () => {
+    if (shouldShowPopup()) {
+        const consent = confirm(cookieBtn)
+        if (consent) {
+            saveToStorage()
+        }
+    }
+}
+
+fetch("http://localhost:3000/" , {})
+.then(()=>{})
+.catch(()=>{
+    console.log("an error occured")
+})
+
+document.addEventListener("submit", (event) => {
     console.log("Validating")
     let user = document.getElementById("username").value;
     let password = document.getElementById("password").value;
 
-  
-    
+
+
     let request = new XMLHttpRequest();
     request.open("POST", "http://localhost:3000/authorize");
     //request.send(new FormData(formElement));
